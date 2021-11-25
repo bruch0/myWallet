37ef22dd933d8c545c37099aebb82f63839c7e31
bruch0 <lucas.bruch0@gmail.com>
2021-11-25 14:08:40 -0300

    refactor: update react-router-dom and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import GlobalStyle from './Styles/GlobalStyles'
-import SignIn from './Pages/SignIn'
-import SignUp from './Pages/SignUp'
-import Home from './Pages/Home'
-import Transaction from './Pages/Transaction'
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyle from './Styles/GlobalStyles';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Home from './Pages/Home';
+import Transaction from './Pages/Transaction';
 
-function App () {
+function App() {
   return (
     <>
-        <GlobalStyle />
-        <BrowserRouter>
-            <Switch>
-                <Route path='/' component={SignIn} exact/>
-                <Route path='/sign-up' component={SignUp} exact/>
-                <Route path='/home' component={Home} exact/>
-                <Route path='/transaction' component={Transaction} exact/>
-            </Switch>
-        </BrowserRouter>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/transaction" element={<Transaction />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
