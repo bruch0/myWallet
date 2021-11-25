cbd4a6199cbac1ef52d1e1b0cbeb3f50bc0c0d51
bruch0 <lucas.bruch0@gmail.com>
2021-11-25 14:10:37 -0300

    refactor: update router-dom, remove validations, use map to render inputs

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,111 +1,115 @@
-import React, { useState } from 'react'
-import { useHistory } from 'react-router'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import {
   Page,
   AppTitle,
   UserInput,
   TryRequest,
-  ChangePage
-} from '../Styles/SignInSignUpStyles'
-import ModalAlert from '../Shared/ModalAlert'
-import axios from 'axios'
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
-import Loader from 'react-loader-spinner'
-import { actionOnEnter, validateSignUp } from '../Shared/functions'
+  ChangePage,
+} from '../Styles/SignInSignUpStyles';
+import { throwError, throwSuccess } from '../Shared/throwAlert';
+import { actionOnEnter } from '../Shared/functions';
+import { signUpUser } from '../Services/api';
+import { validateSignUp } from '../Shared/inputValidations';
 
-function SignIn () {
-  const [userInfo, setUserInfo] = useState({ name: '', email: '', password: '', repeatPassword: '' })
-  const [loading, setLoading] = useState(false)
-  const history = useHistory()
-
-  const loginOnEnter = (key) => {
-    if (actionOnEnter(key)) {
-      validateInputs()
-    }
-  }
+function SignIn() {
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const validateInputs = () => {
-    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const validInputs = validateSignUp({ userInfo });
 
-    const isValid = validateSignUp(userInfo)
-
-    if (isValid) {
-      setLoading(true)
-      axios.post('http://localhost:4000/sign-up', { name: userInfo.name, email: userInfo.email, password: userInfo.password })
+    if (validInputs) {
+      setLoading(true);
+      signUpUser({
+        name: userInfo.name,
+        email: userInfo.email,
+        password: userInfo.password,
+      })
         .then(() => {
-          history.push('/')
+          throwSuccess({ title: 'Conta cadastrada com sucesso' });
+          navigate('/');
         })
         .catch((error) => {
-          setLoading(false)
+          setLoading(false);
           if (error.response.status === 400) {
-            ModalAlert({ title: 'A senha deve conter 5 caracteres ou mais' })
+            throwError({ title: 'A senha deve conter 5 caracteres ou mais' });
           } else if (error.response.status === 409) {
-            ModalAlert({ title: 'Email já cadastrado' })
+            throwError({ title: 'Email já cadastrado' });
           }
-        })
-    } else {
-      if (!userInfo.name.length > 0) {
-        ModalAlert({ title: 'Insira um nome de usuário' })
-      } else if (!userInfo.email.length > 0) {
-        ModalAlert({ title: 'Insira um email' })
-      } else if (!regexEmail.test(userInfo.email) > 0) {
-        ModalAlert({ title: 'Insira um email válido' })
-      } else if (!userInfo.password.length > 0) {
-        ModalAlert({ title: 'Insira uma senha' })
-      } else {
-        ModalAlert({ title: 'As senhas não conferem' })
-      }
+        });
     }
-  }
+  };
+
+  const signUpOnEnter = (key) => {
+    if (actionOnEnter(key)) {
+      validateInputs();
+    }
+  };
+
+  const inputs = [
+    {
+      placeholder: 'Nome',
+      type: 'text',
+      value: userInfo.name,
+      onChange: (input) =>
+        setUserInfo({ ...userInfo, name: input.target.value }),
+    },
+    {
+      placeholder: 'E-mail',
+      type: 'email',
+      value: userInfo.email,
+      onChange: (input) =>
+        setUserInfo({ ...userInfo, email: input.target.value }),
+    },
+    {
+      placeholder: 'Senha',
+      type: 'password',
+      value: userInfo.password,
+      onChange: (input) =>
+        setUserInfo({ ...userInfo, password: input.target.value }),
+    },
+    {
+      placeholder: 'Confirme a senha',
+      type: 'password',
+      value: userInfo.repeatPassword,
+      onChange: (input) =>
+        setUserInfo({ ...userInfo, repeatPassword: input.target.value }),
+    },
+  ];
+
   return (
     <Page>
-        <AppTitle>MyWallet</AppTitle>
+      <AppTitle>MyWallet</AppTitle>
+      {inputs.map((input) => (
         <UserInput
-            placeholder='Nome'
-            type='text'
-            value={userInfo.name}
-            onChange={(input) => setUserInfo({ ...userInfo, name: input.target.value })}
-            onKeyUp={(keyboard => loginOnEnter(keyboard.nativeEvent.key))}
-            loading={loading ? 1 : 0}
+          placeholder={input.placeholder}
+          type={input.type}
+          value={input.value}
+          onChange={input.onChange}
+          onKeyUp={(keyboard) => signUpOnEnter(keyboard.nativeEvent.key)}
+          loading={loading ? 1 : 0}
+          key={input.placeholder}
         />
-        <UserInput
-            placeholder='E-mail'
-            type='email'
-            value={userInfo.email}
-            onChange={(input) => setUserInfo({ ...userInfo, email: input.target.value })}
-            onKeyUp={(keyboard => loginOnEnter(keyboard.nativeEvent.key))}
-            loading={loading ? 1 : 0}
-        />
-        <UserInput
-            placeholder='Senha'
-            type='password'
-            value={userInfo.password}
-            onChange={(input) => setUserInfo({ ...userInfo, password: input.target.value })}
-            onKeyUp={(keyboard => loginOnEnter(keyboard.nativeEvent.key))}
-            loading={loading ? 1 : 0}
-        />
-        <UserInput
-            placeholder='Confirme a senha'
-            type='password'
-            value={userInfo.repeatPassword}
-            onChange={(input) => setUserInfo({ ...userInfo, repeatPassword: input.target.value })}
-            onKeyUp={(keyboard => loginOnEnter(keyboard.nativeEvent.key))}
-            loading={loading ? 1 : 0}
-        />
-        <TryRequest onClick={() => validateInputs()}>
-            {loading
-              ? <Loader
-                    type="ThreeDots"
-                    color="#FFFFFF"
-                    height={100}
-                    width={100}
-                />
-              : 'Cadastrar'
-            }
-        </TryRequest>
-        <ChangePage to='/'>Já tem uma conta? Entre agora!</ChangePage>
+      ))}
+      <TryRequest onClick={validateInputs}>
+        {loading ? (
+          <Loader type="ThreeDots" color="#FFFFFF" height={100} width={100} />
+        ) : (
+          'Cadastrar'
+        )}
+      </TryRequest>
+      <ChangePage to="/">Já tem uma conta? Entre agora!</ChangePage>
     </Page>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
